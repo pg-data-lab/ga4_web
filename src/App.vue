0bfa9be2262b896473c5138bd8a13612c43ef839
etsuro fujita <emba@etsuronoMacBook-Air.local>
2022-05-26 15:04:49 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,93 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h2>カンタン１分で完了！</h2>
+    <div id="app">
+      <div class="form-name">
+          <p>会社<span>*必須</span></p>
+          <input type="text" company="message" placeholder="例) 株式会社⚪︎⚪︎⚪︎⚪︎" v-model="company">
+      </div>
+      <div class="form-name">
+          <p>名前</p>
+          <input type="text" name="name" placeholder="お名前" v-model="name">
+      </div>
+      <div class="form-email">
+          <p>メールアドレス</p>
+          <input type="text" name="email" placeholder="例) sample@sample.jp" v-model="email">
+      </div>
+
+      <div class="form-phone">
+          <p>電話番号</p>
+          <input type="text" name="phone" placeholder="00000000000" v-model="phone">
+      </div>
+    </div>
+    <div>
+      <button @click="increment">{{count}}</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
+export default ({
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      company: "",
+      name: "",
+      email: "",
+      phone: "",
+      count: 0,
+
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      this.count--;
+    },
+  },
+});
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container{
+    text-align:center;
+}
+
+#app{
+    text-align:center;
+}
+
+.form-name,.form-phone,.form-email{
+    width:80%;
+    margin:0 auto;
+    padding:15px 0;
+    margin-bottom:30px;
+    background-color:whitesmoke;
+}
+
+input{
+    padding:5px 10px;
+    min-width:250px;
+}
+
+.error-name,.error-phone,.error-email{
+    color:red;
+    font-weight:bold;
+    text-align:center;
+}
+
+button{
+    width:80%;
+    background-color: teal;
+    color:white;
+    padding:20px 0;
+    border:none;
+    border-radius:10px;
+    cursor:pointer;
+    margin-bottom: 50px;
 }
 </style>
+
+    
