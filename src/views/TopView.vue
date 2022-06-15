306f2aa7184e47e511cd3ddc7c32ed7782ce1e2d
etsuro fujita <emba@etsuronoMacBook-Air-2.local>
2022-06-15 19:05:27 +0900

    デザイン修正, ヘッター・フッターの画像変更

--- src/views/TopView.vue
+++ src/views/TopView.vue
@@ -1,42 +1,42 @@
 <template>
-<header class="justify-between items-center py-4 min-w-full">
+<header class="justify-between items-center py-3 min-w-full">
   <img src="@/assets/PG_rd2.png" alt="Logo" class="text-gray-800 text-2xl lg:text-3xl font-bold text-center mx-auto">
 </header>
-<div class="px-4 md:px-8 mx-auto mb-10 main mt-8">
+<div class="px-4 md:px-8 mx-auto mb-10 main mt-8 max-w-screen-2xl">
   <div class="bg-white">
     <div class="grid lg:grid-cols-5 md:grid-cols-4 grid-cos-1 gap-12 xl:mx-32 lg:mx-20 sm:mx-10 md:mx-10 mx-4">
       
       <div class="left w-full lg:col-span-3 md:col-span-2 cos-span-1 sm:text-left text-center">
         <div class="sm:contents hidden">
           <p class="lg:tracking-widest glay text-xl mb-3">サービス資料ダウンロードページ</p>
-          <h1 class="xl:tracking-widest tracking-wide glay text-3xl">Google Analytics 4がもっとわかる</h1>
-          <p class="lg:tracking-widest glay text-sm mt-3">本フォーム入力後、メールアドレスあてに自動返信メールにて、</p>
+          <h1 class="tracking-base glay text-3xl">Google Analytics 4 がもっとわかる</h1>
+          <p class="lg:tracking-widest glay text-sm mt-4">本フォーム入力後、メールアドレスあてに自動返信メールにて、</p>
           <p class="lg:tracking-widest glay text-sm mb-8">すぐに資料を添付させて頂きます。</p>
         </div>
 
         <div class="sm:hidden contents">
-          <h1 class="xl:tracking-widest tracking-wide glay text-3xl">Google Analytics 4が</h1>
+          <h1 class="xl:tracking-widest tracking-base glay text-3xl">Google Analytics 4 が</h1>
           <h1 class="xl:tracking-widest tracking-wide glay text-3xl">もっとわかる</h1>
           <p class="lg:tracking-widest glay text-sm mt-3">本フォーム入力後、</p>
           <p class="lg:tracking-widest glay text-sm">メールアドレスあてに自動返信メールにて、</p>
           <p class="lg:tracking-widest glay text-sm mb-8">すぐに資料を添付させて頂きます。</p>
         </div>
         
-        <img src="@/assets/ga4_main.png" class="text-gray-800  text-center mx-auto sdw block rds">
+        <img src="@/assets/ga4_main.png" class="text-gray-800 w-max text-center mx-auto sdw block rds">
         
         <div class="mt-5">
-          <ul class="text-gray-500 md:text-lg w-max  grid grid-cos-1 mx-auto">
-            <h1 class="glay tracking-widest orange text-2xl mb-2 cos-span-1"><img src="@/assets/light_icon.png" alt="Logo" class="mb-2.5 mr-2 inline-block">この資料でわかること</h1>
-            <li class="glay sm:tracking-widest tracking-wide cos-span-1">・GoogleAnalytics 4とは</li>
-            <li class="glay sm:tracking-widest tracking-wide cos-span-1">・GoogleAnalytics 4の5つの変更点</li>
-            <li class="glay sm:tracking-widest tracking-wide cos-span-1">・GA4導入を今すぐ行う必要性</li>
-            <li class="glay sm:tracking-widest tracking-wide cos-span-1">・導入サポートの料金費用</li>
-            <li class="glay sm:tracking-widest tracking-wide cos-span-1">・ご発注から計測開始までのスケジュール</li>
+          <ul class="text-gray-500 md:text-lg w-max">
+            <h1 class="glay tracking-widest orange text-2xl mb-2"><img src="@/assets/light_icon.png" alt="Logo" class="mb-2.5 mr-2 inline-block">この資料でわかること</h1>
+            <li class="glay sm:tracking-widest tracking-wide">・Google Analytics 4 とは</li>
+            <li class="glay sm:tracking-widest tracking-wide">・Google Analytics 4 の5つの変更点</li>
+            <li class="glay sm:tracking-widest tracking-wide">・GA4導入を今すぐ行う必要性</li>
+            <li class="glay sm:tracking-widest tracking-wide">・導入サポートの料金費用</li>
+            <li class="glay sm:tracking-widest tracking-wide">・ご発注から計測開始までのスケジュール</li>
           </ul>
         </div>
       </div>
 
-      <div class="right px-4 pt-14 pb-4 lg:col-span-2 md:col-span-2 cos-span-1 md:h-min mx-auto md:w-full sm:w-10/12 sdw">
+      <div class="right px-4 pt-14 pb-4 lg:col-span-2 md:col-span-2 cos-span-1 md:h-min mx-auto w-auto sdw">
         <!-- form - start -->
         <form class="max-w-screen-md grid sm:grid-cols-2 gap-4 mx-auto">
           <div class="sm:col-span-2">
@@ -45,19 +45,19 @@
             <p v-if="errors['company']" class="need">{{errors['company']}}</p>
           </div>
 
-          <div class="sm:col-span-2">
+          <div class="sm:col-span-2 mt-4">
             <label for="name" class="tracking-wider font-label font-bold inline-block text-sm sm:text-base mb-2"><span class="need bg-red-50	text-sm sm:text-base py-1 px-4 mr-2 rounded">必須</span>氏名</label>
             <input id="name" name="name" v-model="name" type="text" placeholder="例)  お名前" class="w-full rounded-lg border-gray-300 border-2 placeholder-gray-300" />
             <p v-if="errors['name']" class="need">{{errors['name']}}</p>
           </div>
 
-          <div class="sm:col-span-2">
+          <div class="sm:col-span-2 mt-4">
             <label for="email" class="tracking-wider font-label font-bold inline-block text-sm sm:text-base mb-2"><span class="need bg-red-50	text-sm sm:text-base py-1 px-4 mr-2 rounded">必須</span>メールアドレス</label>
             <input id="email" name="email" v-model="email" type="email" placeholder="例)  sample@project-g.co.jp" class="w-full rounded-lg border-gray-300 border-2 placeholder-gray-300" />
             <p v-if="errors['email']" class="need">{{errors['email']}}</p>
           </div>
 
-          <div class="sm:col-span-2">
+          <div class="sm:col-span-2 mt-4">
             <label for="phone" class="tracking-wider font-label font-bold inline-block text-sm sm:text-base mb-2"><span class="need bg-red-50	text-sm sm:text-base py-1 px-4 mr-2 rounded">必須</span>電話番号</label>
             <input id="phone" name="phone" v-model="phone" type="text" placeholder="例)  0362069608" class="w-full rounded-lg border-gray-300 border-2 placeholder-gray-300 " />
             <p v-if="errors['phone']" class="need">{{errors['phone']}}</p>
@@ -71,7 +71,7 @@
             </div>
           </div>
           <div class="sm:col-span-2 mx-3 text-center">
-            <input type="button" @click="getForm" value="資料をダウンロード" class="tracking-widest w-full bg-yellow-200 hover:bg-yellow-300 active:bg-yellow-500 focus-visible:ring ring-gray-300 text-black xl:text-2xl lg:text-lg md:text-base sm:text-lg text-sm font-semibold rds sdw outline-none transition duration-100 px-8 py-3 mx-auto">
+            <input type="button" @click="getForm" value="資料をダウンロード" class="tracking-widest w-full bg-yellow-200 hover:bg-yellow-300 active:bg-yellow-500 focus-visible:ring ring-gray-300 text-black text-base font-semibold rds sdw outline-none transition duration-100 px-8 py-3 mx-auto">
           </div>
         </form>
         <!-- form - end -->
